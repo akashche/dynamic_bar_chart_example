de9eab96b03971da1839dbb9c665afd89f5494ea
akashche <akashche@redhat.com>
2015-08-04 10:37:06 +0300

    bootstrap ui added, works with agent

---
+++ README.md
@@ -0,0 +1,18 @@
+Example Web Application with Dynamic Chart
+==========================================
+
+This project implements a dynamic bars chart that shows number of live JVM threads.
+It is a sub-project of [jvmti_http](https://github.com/akashche/jvmti_http) project and
+should be used from the JVM TI agent.
+
+License information
+-------------------
+
+This project is released under the [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Changelog
+---------
+
+**2015-08-04**
+
+ * initial public version
--- public_html/lib/com/redhat/akashche/BarChartPanel.js
+++ public_html/lib/com/redhat/akashche/BarChartPanel.js
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2015, akashche at redhat.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 define([
     // common
     "dojo/_base/declare",
     "dojo/_base/lang",
     "dojo/_base/array",
-    "dojox/lang/functional/object",
+    "dojo/request/xhr",
     // charts
     "dojox/charting/Chart",
     "dojox/charting/plot2d/Columns",
@@ -16,28 +31,49 @@ define([
     // wait for DOM
     "dojo/domReady!"
 ], function (
-        declare, lang, array, object,// common
+        declare, lang, array, xhr, // common
         Chart, Columns, Grid, theme, Default, // charts
         utils // local
     ) {
     return declare([], {
         constructor: function (conf) {
             this.conf = utils.mixinDeep(this._defaultConf(), conf);
+            this.data = [];
+            for (var i = 0; i < 24; i++) {
+                this.data.push(1);
+            }
             this.chart = new Chart(conf.renderTo)
                     .addAxis("bottom", this.conf.bottomAxis)
                     .addAxis("left", this.conf.leftAxis)
                     .addPlot("default", this.conf.columns)
                     .addPlot("grid", this.conf.grid)
                     .setTheme(theme)
-                    .addSeries("default", [1, 2, 3, 4, 5])
+                    .addSeries("default", this.data)
                     .render();
-            this.flag = true;
+            for (var i = 0; i < 24; i++) {
+                this.data[i] = 0;
+            }
+            this.chart.updateSeries("default", this.data).render();
             setInterval(lang.hitch(this, function () {
-                var data = this.flag ? [5, 4, 3, 2, 1] : [1, 2, 3, 4, 5];
-                this.flag = !this.flag;
-                this.chart.updateSeries("default", data);
-                this.chart.render();
-            }), 2000);
+                xhr("/jvmti/GetLiveThreadsCount/").then(lang.hitch(this, function(count) {
+                    this.data.push(parseInt(count, 10));
+                    this.data = this.data.slice(1);
+                    this.chart.updateSeries("default", this.data);
+                    this.chart.render();
+                }), console.log);
+            }), 2100);
+            xhr("/jvmti/GetSystemProperty/java.home").then(function(str) {
+                document.getElementById("javaHomeField").appendChild(document.createTextNode(str));
+            }, console.log);
+            xhr("/jvmti/GetSystemProperty/java.vm.name").then(function (str) {
+                document.getElementById("javaVmNameField").appendChild(document.createTextNode(str));
+            }, console.log);
+            xhr("/jvmti/GetSystemProperty/java.vm.version").then(function (str) {
+                document.getElementById("javaVmVersionField").appendChild(document.createTextNode(str));
+            }, console.log);
+            xhr("/jvmti/GetSystemProperty/java.vm.vendor").then(function (str) {
+                document.getElementById("javaVmVendorField").appendChild(document.createTextNode(str));
+            }, console.log);            
         },
         _defaultConf: function () {
             return {
@@ -50,20 +86,20 @@ define([
                     minorLabels: false,
                     majorTicks: true,
                     minorTicks: false,
-                    majorTickStep: 1   
+                    majorTickStep: 1
                 },
                 leftAxis: {
                     vertical: true,
                     fixLower: "major",
                     fixUpper: "major",
                     includeZero: true,
-                    majorTickStep: 1,
+                    majorTickStep: 5,
                     majorLabels: true,
                     minorLabels: false,
                     majorTicks: true,
                     minorTicks: false,
                     maxLabelSize: 30,
-                    title: "some label",
+                    title: "",
                     htmlLabels: false,
                     startFromZero: true
                 },
